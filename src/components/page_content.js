82a291ea84b8a991026bdbcd147c71e65ff90d1c
Leonardo de miranda Borba <borba@gogood.com.br>
2021-03-09 18:18:28 -0300

    fix: scrollview correct

--- src/components/page_content.js
+++ src/components/page_content.js
@@ -3,9 +3,11 @@ import { View, ScrollView } from 'react-native';
 import style from '../styles/content';
 
 export const Page = (props) => (
-  <ScrollView style={style.pageContainer}>{props.children}</ScrollView>
+  <View style={style.pageContainer}>{props.children}</View>
 );
 
 export const Content = (props) => {
-  return <View style={style.contentContainer}>{props.children}</View>;
+  return (
+    <ScrollView style={style.contentContainer}>{props.children}</ScrollView>
+  );
 };
