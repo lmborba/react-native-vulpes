d41f5796b48cf5aaa30b0173258dd1c1f58b6971
Leonardo de Miranda Borba <borba@gogood.com.br>
2021-06-14 18:50:15 -0300

    fix: padded view

--- src/components/utils.js
+++ src/components/utils.js
@@ -6,7 +6,10 @@ const fillSpaceStyle = { flex: 1 };
 export const FillSpace = (props) => <View style={fillSpaceStyle} />;
 
 export const PaddedView = ({ style, children, restProps }) => (
-  <View {...restProps} style={style}>
+  <View {...restProps} style={addPaddingsToOriginalStyle(style)}>
     {children}
   </View>
 );
+function addPaddingsToOriginalStyle(style) {
+  return { ...style, paddingLeft: 16, paddingRight: 16 };
+}
