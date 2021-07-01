a6a270dffc638ea9c87f8a8b20cf03fe0980d8f0
Leonardo de Miranda Borba <borba@gogood.com.br>
2021-07-01 01:34:59 -0300

    feat: menu

--- src/components/menu.js
+++ src/components/menu.js
@@ -3,25 +3,22 @@ import { TouchableOpacity, View } from 'react-native';
 import { Colors } from '../colors';
 import { Fonts } from '../fonts';
 
-export const Menu = (props) => {
-  return (
-    <View
-      style={{
-        flexDirection: 'Row',
-        borderTopWidth: 1,
-        backgroundColor: Colors.white,
-        borderTopColor: Colors.light_gray,
-        paddingBottom: 9,
-      }}
-    >
-      {props.children}
-    </View>
-  );
+const generalMenuStyle = {
+  flexDirection: 'Row',
+  borderTopWidth: 1,
+  backgroundColor: Colors.white,
+  borderTopColor: Colors.light_gray,
+  paddingBottom: 9,
 };
+export const Menu = (props) => {
+  return <View style={generalMenuStyle}>{props.children}</View>;
+};
+
+const generalMenuItemStyle = { flex: 1, padding: 15 };
 
 export const MenuItem = ({ children, selected, onPress }) => {
   return (
-    <TouchableOpacity style={{ flex: 1, padding: 15 }} onPress={onPress}>
+    <TouchableOpacity style={generalMenuItemStyle} onPress={onPress}>
       {React.Children.map(children, (child, i) => {
         if (React.isValidElement(child)) {
           return React.cloneElement(child, {
