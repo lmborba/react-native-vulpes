80e30b16358a2b6af252b49a8ada8586a5464781
Leonardo de Miranda Borba <borba@gogood.com.br>
2021-08-31 12:53:23 -0300

    fix: small Change

--- src/components/page_content.js
+++ src/components/page_content.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FlatList, ScrollView, View, RefreshControl } from 'react-native';
+import { FlatList, RefreshControl, ScrollView, View } from 'react-native';
 import style from '../styles/content';
 import { H4 } from './typos';
 
@@ -43,32 +43,28 @@ const SepItemDefault = () => {
 
 export const ContentList = ({ noPadding, style: customStyle, ...props }) => {
   let completeStyle = style.contentContainer;
+
   if (noPadding) {
     completeStyle = {
       ...completeStyle,
       ...style.noPadding,
     };
   }
+
   completeStyle = { ...completeStyle, ...customStyle };
 
-  let header = () => {
-    return (
-      <View>
-        {props.title && <H4 style={style.contentTitle}>{props.title}</H4>}
-        {props.ListHeaderComponent}
-      </View>
-    );
-  };
-
   return (
-    <FlatList
-      contentContainerStyle={style.contentContainerList}
-      style={completeStyle}
-      ItemSeparatorComponent={SepItemDefault}
-      {...props}
-      ListHeaderComponent={header.bind(this)}
-    >
-      {props.children}
-    </FlatList>
+    <View>
+      {props.title && <H4 style={style.contentTitle}>{props.title}</H4>}
+
+      <FlatList
+        contentContainerStyle={style.contentContainerList}
+        style={completeStyle}
+        ItemSeparatorComponent={SepItemDefault}
+        {...props}
+      >
+        {props.children}
+      </FlatList>
+    </View>
   );
 };
