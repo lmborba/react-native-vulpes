8ccbbb4e2ff01e468a4e3893c109f22e53641933
Fernando Stefanello <fernandostefa@gmail.com>
2021-12-13 15:35:32 -0300

    fix: spinner props

--- src/components/spinner.js
+++ src/components/spinner.js
@@ -4,5 +4,7 @@ import { Colors } from '../colors';
 
 export const Spinner = ({ color, size, ...restProps }) => {
   const colorVal = color ? Colors[color] : Colors.cyan;
-  return <ActivityIndicator size={size || 'small'} color={colorVal} />;
+  return (
+    <ActivityIndicator size={size || 'small'} color={colorVal} {...restProps} />
+  );
 };
