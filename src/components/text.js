02a383d4aaabfc0520af3d5ac01b709e55c0f3cc
scostadavid <davidshcosta@gmail.com>
2022-06-24 11:52:25 -0300

    feat: add underline prop to text

--- src/components/text.js
+++ src/components/text.js
@@ -3,7 +3,16 @@ import { Text as NativeText } from 'react-native';
 import { getColors } from '../colors';
 import useVulpes from '../hooks/useVulpes';
 
-const Text = ({ style, title, fontStyle, color, center, strike, ...props }) => {
+const Text = ({
+  style,
+  title,
+  fontStyle,
+  color,
+  center,
+  strike,
+  underline,
+  ...props
+}) => {
   const { theme } = useVulpes();
   const colors = getColors(theme);
   const colorStyle = {
@@ -17,6 +26,7 @@ const Text = ({ style, title, fontStyle, color, center, strike, ...props }) => {
     ...style,
     ...(center ? { textAlign: 'center' } : {}),
     ...(strike ? { textDecorationLine: 'line-through' } : {}),
+    ...(underline ? { textDecorationLine: 'underline' } : {}),
   };
 
   if (props.testin) {
