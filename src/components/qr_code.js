f0dec850fbba802df2128960388935b32438df23
MarceloArraes <marcelo.arraes@gmail.com>
2022-08-01 14:43:28 -0300

    fix: on qrcode error correction higher

--- src/components/qr_code.js
+++ src/components/qr_code.js
@@ -30,6 +30,7 @@ export class QRCodeValid extends Component {
           <QRCode
             value={code}
             color={colors(color)}
+            ecl={'H'}
             size={defSize}
             logoMargin={2}
             logo={logo}
